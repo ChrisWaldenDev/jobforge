ca277a7e9980938cbb9714871d12f42e0a780325
Chris Walden <chriswalden80@gmail.com>
2026-03-10 21:10:27 -0400

    Fixed PGSQL issue

--- src/main/java/dev/chriswalden/jobforge/core/domain/CsvFile.java
+++ src/main/java/dev/chriswalden/jobforge/core/domain/CsvFile.java
@@ -18,14 +18,15 @@ public class CsvFile {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+    @Column(name = "original_name")
     private String originalName;
+    @Column(name = "content_type")
     private String contentType;
 
-    @Lob
     @Column(nullable = false)
     private byte[] data;
 
-    @Column(nullable = false, updatable = false)
+    @Column(nullable = false, updatable = false, name = "created_at")
     private Instant createdAt;
 
     @PrePersist
